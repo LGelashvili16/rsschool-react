5c73d1ed1ebd3641b74cb30ec602e384e0c78b42
Lado Gelashvili <ladogelashvili16@gmail.com>
2024-07-04 01:01:10 +0400

    implement local storage logic

--- src/components/Search.tsx
+++ src/components/Search.tsx
@@ -1,18 +1,57 @@
 import React, { Component } from "react";
+import classes from "./Search.module.css";
 
-class Search extends Component {
+type SearchState = {
+  searchTerm: string;
+};
+class Search extends Component<null, SearchState> {
+  state = {
+    searchTerm: "",
+  };
+
+  componentDidMount(): void {
+    const searchedTerm = localStorage.getItem("searchTerm");
+
+    if (searchedTerm !== null) {
+      this.setState({
+        searchTerm: JSON.parse(searchedTerm),
+      });
+    } else {
+      this.setState({
+        searchTerm: "",
+      });
+    }
+  }
+
   searchHandler = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
-    console.log("Hi");
+    const formData = new FormData(e.target as HTMLFormElement);
+    const inputValue = formData.get("searchTerm");
+
+    if (typeof inputValue === "string") {
+      localStorage.setItem("searchTerm", JSON.stringify(inputValue.trim()));
+      // this.setState({ searchTerm: inputValue });
+    }
+  };
+
+  inputChangeHandler = (e: React.ChangeEvent<HTMLInputElement>) => {
+    this.setState({ searchTerm: e.target.value });
   };
 
   render() {
     return (
-      <section>
-        <form onSubmit={this.searchHandler}>
-          <input type="search" />
+      <section className={classes["search-section"]}>
+        <form className={classes["search-form"]} onSubmit={this.searchHandler}>
+          <input
+            type="search"
+            name="searchTerm"
+            placeholder="Type Search Term"
+            value={this.state.searchTerm}
+            onChange={this.inputChangeHandler}
+          />
           <button>Search</button>
         </form>
+        {this.state.searchTerm}
       </section>
     );
   }
